d1ddafb643b3c87823e88d445f3b5d85d09d2c05
KwekuYamoah <kwekuandyamoah@gmail.com>
2021-05-08 12:12:09 +0000

    Fixed active class in header

--- inc/nav.php
+++ inc/nav.php
@@ -8,11 +8,42 @@
 
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
-                <li><a class="active" href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="courses.php">Courses</a></li>
-                <li><a href="trainers.php">Trainers</a></li>
-                <li><a href="events.php">Events</a></li>
+                
+                <li><a class=<?php
+                                if($title == "Home|Ashesi Tutorial Hub"){
+                                    echo "active";
+                                }
+                                else{
+                                    echo "null";}
+                            ?> href="index.php">Home</a></li>
+                <li><a class=<?php
+                                if($title == "About|Ashesi Tutorial Hub"){
+                                    echo "active";
+                                }
+                                else{
+                                    echo "null";}
+                            ?> href="about.php">About</a></li>
+                <li><a class=<?php
+                                if($title == "Courses|Ashesi Tutorial Hub"){
+                                    echo "active";
+                                }
+                                else{
+                                    echo "null";}
+                            ?>  href="courses.php">Courses</a></li>
+                <li><a class=<?php
+                                if($title == "Trainers|Ashesi Tutorial Hub"){
+                                    echo "active";
+                                }
+                                else{
+                                    echo "null";}
+                            ?> href="trainers.php">Trainers</a></li>
+                <li><a class=<?php
+                                if($title == "Events|Ashesi Tutorial Hub"){
+                                    echo "active";
+                                }
+                                else{
+                                    echo "null";}
+                            ?> href="events.php">Events</a></li>
                 
 
                 <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
@@ -32,7 +63,13 @@
                 <li><a href="#">Drop Down 4</a></li>
             </ul>
                 </li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a class=<?php
+                                if($title == "Contact|Ashesi Tutorial Hub"){
+                                    echo "active";
+                                }
+                                else{
+                                    echo "null";}
+                            ?> href="contact.php">Contact</a></li>
             </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
