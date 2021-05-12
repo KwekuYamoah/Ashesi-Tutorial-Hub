7ceab7f942809cfca423cdef02b0cc1b87eeef22
KwekuYamoah <kwekuandyamoah@gmail.com>
2021-05-12 20:40:49 +0000

    added heroku cleardb

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fc673b65034249a572ec63ba338aac5b38bd8993',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc673b65034249a572ec63ba338aac5b38bd8993',
+    ),
+  ),
+);
