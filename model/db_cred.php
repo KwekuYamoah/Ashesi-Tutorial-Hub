05e0a7ef63f3a87f5d689f99fc8bca788ad58d58
KwekuYamoah <kwekuandyamoah@gmail.com>
2021-05-12 19:23:06 +0000

    Made changes for deployment

--- model/db_cred.php
+++ model/db_cred.php
@@ -1,7 +1,18 @@
 <?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
 //databse credentials
-define("DATABASE","ASHTUTOR");
-define("SERVER","localhost");
-define("USERNAME","root");
-define("PASSWD",getenv("MYSQL_ROOT") ?? "");
+define("DATABASE","ASHTUTOR" ?? $cleardb_db);
+define("SERVER","localhost"?? $cleardb_server);
+define("USERNAME","root" ?? $cleardb_username);
+define("PASSWD","" ?? $cleardb_password );
+
 ?>
