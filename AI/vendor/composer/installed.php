03f346bd272842d1d1765d1a89262abea6c674bb
Brown <persiebrown285@gmail.com>
2021-05-13 19:23:07 +0000

    Niiknow Bayes Test

---
+++ AI/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '65a95367e47fa5e5b8af3b5b48d5c3d7fe537719',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '65a95367e47fa5e5b8af3b5b48d5c3d7fe537719',
+    ),
+    'niiknow/bayes' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e754145d52900fdf4c1e620cc561132954721cf9',
+    ),
+  ),
+);
