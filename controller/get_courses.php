1d974255601e01e5c30a50c2fc272519d4b5604a
KwekuYamoah <kwekuandyamoah@gmail.com>
2021-05-14 06:10:45 +0000

    Finished adding courses

--- controller/get_courses.php
+++ controller/get_courses.php
@@ -1,4 +1,24 @@
 <?php
 require_once "../model/connect_db.php";
 session_start();
+    //functio to return json of courses
+    function json_courses(){
+        $db = connect_db();
+
+        $sql = "SELECT vidTopic,vidUrl FROM video";
+        $result = mysqli_query($db,$sql);
+        // If result matched $myemail and $mypassword, table row must be 1 row
+        $rows = array();
+         
+        while($row = mysqli_fetch_assoc($result)) {
+            
+            $rows[] = $row;
+        }
+        
+        return json_encode($rows);
+        
+    }
+
+    
 ?>
+
--- view/courses.php
+++ view/courses.php
@@ -8,6 +8,8 @@
   include_once("../inc/nav.php");
   session_start();
 
+  require_once("../controller/get_courses.php");
+
 ?>
   <main id="main" data-aos="fade-in">
 
@@ -25,78 +27,29 @@
 
         <div class="row" data-aos="zoom-in" data-aos-delay="100">
 
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="course-item">
-            <iframe width="100%" height="60%" src="http://www.youtube.com/embed/n_dZNLr2cME?rel=0&hd=1" frameborder="0" allowfullscreen></iframe>
-              <div class="course-content">
-                
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="../assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
-                    <span>Antonio</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;50
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;65
-                  </div>
+          <?php
+            $json_data = json_courses();
+
+            $decoded_data = json_decode($json_data,true);
+          
+            foreach($decoded_data as $item) {
+
+              echo '<div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div class="course-item">
+              <iframe width="100%" height="60%" src=';echo $item["vidUrl"]; echo" ". 'frameborder="0" allowfullscreen></iframe>
+                <div class="course-content">
+                  
+                  <p>'; echo $item["vidTopic"];
+                  echo '</p>
+                  
                 </div>
               </div>
-            </div>
-          </div> <!-- End Course Item-->
+            </div> <!-- End Course Item-->';
+            }
+          ?>
 
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-            <div class="course-item">
-              <img src="../assets/img/course-2.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Marketing</h4>
-                  <!-- <p class="price">$250</p> -->
-                </div>
 
-                <h3><a href="course-details.php">Search Engine Optimization</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="../assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
-                    <span>Lana</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;35
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;42
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> <!-- End Course Item-->
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-            <div class="course-item">
-              <img src="../assets/img/course-3.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Content</h4>
-                  <!-- <p class="price">$180</p> -->
-                </div>
-
-                <h3><a href="course-details.php">Copywriting</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="../assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
-                    <span>Brandon</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;20
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;85
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> <!-- End Course Item-->
+          
 
         </div>
 
@@ -107,6 +60,7 @@
 
 
 <?php
+ 
   //include footer
   include_once("../inc/footer.php");
 ?>
